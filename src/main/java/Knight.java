274a6c961bdf0b31a38ea7847b37b726a85d24d8
David9Nagy <divadadd@gmail.com>
2022-11-06 19:57:45 -0400

    knight class implemented

---
+++ src/main/java/Knight.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Knight extends Piece{
+    public int pointValue = 3;
+
+    private Arraylist<Move> generateMoves(){
+        int[] pos;
+        pos = super.getCoordinates(); // get position of knight
+        Arraylist<Move> moves = new Arraylist<Move>();
+        int[][] add = {{2, 1}, {2, -1}, {1, 2}, {1, 2}, {1, -2}, {-1, 2}, {-1, -2}, {-2, 1}, {-2, -1}}; // to be added to pos
+        for(int i = 0; i <= 7; i++) {
+            moves.add(new Move(this, new int[]{pos[0] + add[i][0], pos[1] + add[i][1]})); // adds new Move instance to move
+        }
+        return moves;
+    }
+}
