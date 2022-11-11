174738d3fa09999da218b0f88acba18ce1c706c0
David9Nagy <divadadd@gmail.com>
2022-11-11 19:33:55 -0400

    player updated

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -3,13 +3,19 @@ public class Player {
     public String name;
     public char color;
     public List<Piece> pieces;
-    public int points = 0;
-    public List<Piece> capturedPieces;
+    private int points = 0;
+    private List<Piece> capturedPieces;
 
     public Player (String name, char color) {
         this.name = name;
         this.color = color;
         // implement: get players pieces from board.pieces and store in this.pieces
     }
-    // are we updating points and captured Pieces here or is that happening somewhere else?
+    public void addCapturedPiece (Piece captured) {
+        this.points += captured.pointValue;
+        this.capturedPieces.add(captured);
+    }
+    public List<Piece> getCapturedPieces () {
+        return this.capturedPieces;
+    }
 }
