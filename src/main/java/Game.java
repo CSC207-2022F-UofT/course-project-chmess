7142c5deddba68a208d1bb1e95e047e105f5b28b
Ryan Nguyen <ry.nguyen@mail.utoronto.ca>
2022-11-13 16:35:03 -0500

    Refactor so that Game defers to board for player objects

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,36 +1,36 @@
 public class Game {
     private Board board;
-    private Player[] players;
+    private char gameState;
 
     public Game() {}
-
-    public Board getBoard() {
-        return this.board;
-    }
 
     public void setBoard(Board board) {
         this.board = board;
     }
 
-    public Player[] getPlayers() {
-        return this.players;
+    public Board getBoard() {
+        return this.board;
     }
 
     public void setPlayers(Player[] players) {
-        this.players = players;
+        this.board.setPlayers(players);
+    }
+
+    public Player[] getPlayers() {
+        return this.board.getPlayers();
+    }
+
+    public void setGameState(char gameState) {
+        this.gameState = gameState;
     }
 
     /**
      * Returns a char representing the game state.
      * This should be '$' for stalemate or '#' for checkmate.
      * No particular character is designated for the case of neither.
-     *
      * Note: Should maybe be replaced with isStalemate, isCheckmate
      */
     public char getGameState() {
-        // Note: Closely related to seeing if King is in check
-        // both maybe should be moved to their own classes
-        // TODO
-        return '$';
+        return this.gameState;
     }
 }
