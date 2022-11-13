e65d7127ca38736409037a83518f6dbc7a7a1312
Ryan Nguyen <ry.nguyen@mail.utoronto.ca>
2022-11-13 14:06:11 -0500

    Create BoardCopier class

---
+++ src/main/java/BoardCopier.java
@@ -0,0 +1,19 @@
+public class BoardCopier {
+    private Board board;
+
+    public BoardCopier(Board board) {
+        this.board = board;
+    }
+
+    /**
+     * Returns a deep clone of the given Board instance.
+     *
+     * @see Board#copy() Board.copy()
+     *
+     * @return a clone of the board
+     */
+    public Board createCopy() {
+        // TODO
+        return null;
+    }
+}
