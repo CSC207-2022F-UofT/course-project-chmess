f06011ab84e1fa6b9d993c9bcfa3da0811934a49
Ryan Nguyen <ry.nguyen@mail.utoronto.ca>
2022-11-13 14:58:12 -0500

    Created CheckChecker class

---
+++ src/main/java/CheckChecker.java
@@ -0,0 +1,14 @@
+class CheckChecker {
+    public CheckChecker() {}
+
+    /**
+     * Returns whether a player is in check.
+     * @param board
+     * @param player
+     * @return whether the given player is in check
+     */
+    public boolean isPlayerInCheck(Board board, Player player) {
+        // TODO
+        return false;
+    }
+}
