c4f7678c1b2c1893a9334ab894696335897eb8d8
David9Nagy <divadadd@gmail.com>
2022-11-13 01:55:40 -0400

    skeletons for MovesGenerator subclasses

---
+++ src/main/java/BishopMovesGen.java
@@ -0,0 +1,15 @@
+public class BishopMovesGen extends MovesGenerator {
+    /**
+     * Constructor creates list of possible moves a bishop
+     * can make which is later refined by PostMoveValidator.
+     * Checks if bishop can move seven squares diagonally in
+     * either direction. Any moves which jump over a piece,
+     * are not on the board, or are occupied by a friendly
+     * piece are removed from the list. King checks are NOT
+     * considered.
+     */
+    public BishopMovesGen (Board board, Piece bishop) {
+        // implementation
+        // calls removeInvalid method from parent class to remove invalid moves
+    }
+}
