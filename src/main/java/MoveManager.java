6ba0c695cc8ffb44e99a8628a2608ab93e3f787e
Ryan Nguyen <ry.nguyen@mail.utoronto.ca>
2022-11-13 16:33:48 -0500

    Create MoveManager class

---
+++ src/main/java/MoveManager.java
@@ -0,0 +1,30 @@
+class MoveManager {
+    public MoveManager() {}
+
+    /**
+     * Makes the given move on the given game.
+     * This includes updating the board reference in Game,
+     * updating players and their points and pieces captured,
+     * and updating the Game state (stalemate/checkmate/neither).
+     * Note: In the current implementation, players are stored only in boards.
+     *
+     * @param game
+     * @param move
+     */
+    public void makeMove(Game game, Move move) {
+        // TODO
+    }
+
+    /**
+     * Undoes the last move on the given game.
+     * This includes updating the board reference in Game,
+     * updating players and their points and pieces captured,
+     * and updating the Game state (stalemate/checkmate/neither).
+     * Note: In the current implementation, players are stored only in boards.
+     *
+     * @param game
+     */
+    public void undoLastMove(Game game) {
+        // TODO
+    }
+}
