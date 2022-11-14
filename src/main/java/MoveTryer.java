f4322512498bb45887bc1ebd068f55498a48fee4
David9Nagy <divadadd@gmail.com>
2022-11-13 23:41:12 -0400

    added skeleton classes

---
+++ src/main/java/MoveTryer.java
@@ -0,0 +1,11 @@
+public class MoveTryer {
+    public Board newBoard;
+    /**
+     * Given a board and a move, creates a deep clone
+     * of that board and makes the specified move.
+     */
+    public MoveTryer (Board oldBoard, Move move) {
+        // uses CloneBoard class to create a new board and
+        // Try method in Move class to make specified move
+    }
+}
---
+++ src/main/java/PostMoveValidator.java
@@ -0,0 +1,16 @@
+import java.util.List;
+public class PostMoveValidator {
+    public List<Move> validMoves;
+    /**
+     * Takes in a board instance and a list of semi valid moves
+     * created by MoveGenerator and removes all invalid moves
+     * due to King checks. In more detail, calls MoveTryer for
+     * each move in semiValidMoves, creating a cloned board for
+     * every move. Calls MovesGenerator on this cloned board for
+     * every piece belonging to opposing player, and if any of these
+     * moves capture the King, the original move is invalid and is removed.
+     */
+    public PostMoveValidator (Board board, List<Move> semiValidMoves) {
+
+    }
+}
