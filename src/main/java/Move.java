f1a3fba358af62f64f89d016ae8c2b21ec3384e2
David9Nagy <divadadd@gmail.com>
2022-11-14 20:48:11 -0400

    move classes

--- src/main/java/Move.java
+++ src/main/java/Move.java
@@ -8,7 +8,6 @@ public class Move {
         this.origin = origin;
         this.destination = destination;
     }
-
     /**
      * Mutates board to reflect 'natural' move as opposed to
      * castling, promoting, etc. Piece located on origin square
