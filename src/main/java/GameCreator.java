ebc22cded30b10ef5cd8c7c70d5823ae68d03839
Ryan Nguyen <ry.nguyen@mail.utoronto.ca>
2022-11-16 18:45:35 -0500

    Implemented GameCreator

--- src/main/java/GameCreator.java
+++ src/main/java/GameCreator.java
@@ -11,7 +11,11 @@ class GameCreator {
     public Game create() {
         Game game = new Game();
 
-        // TODO: Rest of initialization
+        Player p1 = new Player("Player 1", 'W');
+        Player p2 = new Player("Player 2", 'B');
+        BoardCreator bc = new BoardCreator(p1, p2);
+        Board board = bc.create();
+        game.setBoard(board);
 
         return game;
     }
