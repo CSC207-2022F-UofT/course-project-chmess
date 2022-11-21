55e2be4cd5bead2b03d6387298f51cb85d26bdf7
gurma <gurman.sachdeva@mail.utoronto.ca>
2022-11-21 02:30:42 -0500

    Implemented QueenMovesGen, BishopMovesGen, RookMovesGen basic functionality

--- src/main/java/QueenMovesGen.java
+++ src/main/java/QueenMovesGen.java
@@ -1,15 +1,27 @@
+import java.util.ArrayList;
+import java.util.List;
+/**
+ * Contains a single method which creates list of possible moves
+ * a queen can make which is later refined by PostMoveValidator.
+ */
 public class QueenMovesGen extends MovesGenerator {
     /**
-     * Constructor creates list of possible moves a queen
-     * can make which is later refined by PostMoveValidator.
-     * Checks if queen can move seven squares vertically,
-     * horizontally, or diagonally in either direction. Any
-     * moves which jump over a piece, are not on the board,
-     * or are occupied by a friendly piece are removed from
-     * the list. King checks are NOT considered.
+     * Checks if queen can move seven squares along any straight line.
+     * Any moves which jump over a piece,
+     * are not on the board, or are occupied by a friendly
+     * piece are removed from the list. King checks are NOT
+     * considered at this stage.
+     *
+     * @param board the board on which moves are generated
+     * @param queen the queen piece for which moves are generated
+     * @return the list of possible moves
      */
-    public QueenMovesGen (Board board, Piece queen) {
-        // implementation
-        // calls removeInvalid method from parent class to remove invalid moves
+    public List<Move> generate (Board board, Piece queen) {
+        //creates list of moves to be returned and initializes with diagonal moves,
+        //generated using BishopMovesGen class instance
+        List<Move> moveList = new ArrayList<Move>(new BishopMovesGen().generate(board, queen));
+        //next adds all cardinal moves possible, generated via RookMovesGen
+        moveList.addAll(new RookMovesGen().generate(board, queen));
+        return moveList;
     }
 }
