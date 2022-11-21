f28d78859223403599acd832822df9bb82eda2c0
David9Nagy <divadadd@gmail.com>
2022-11-21 17:09:40 -0400

    removed isPawnDouble and isCapture variables

--- src/main/java/Move.java
+++ src/main/java/Move.java
@@ -1,18 +1,16 @@
 public class Move {
     public int[] origin;
     public int[] destination;
-    public boolean isPawnDouble = false;
-    public boolean isCapture = false;
-
+    // isPawnDouble and isCapture is not required
     public Move (int[] origin, int[] destination) {
         this.origin = origin;
         this.destination = destination;
     }
-
     /**
      * Mutates board to reflect 'natural' move as opposed to
      * castling, promoting, etc. Piece located on origin square
-     * is moved to destination square.
+     * is moved to destination square. If destination square
+     * contains an enemy piece that piece is captured.
      */
     public void Try(Board board) {
 
