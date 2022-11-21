8da02df99338e3acdcd90ec581cecc361d275885
David9Nagy <divadadd@gmail.com>
2022-11-21 17:03:06 -0400

    changes to movegen and implemented KnightMovesGen

--- src/main/java/KnightMovesGen.java
+++ src/main/java/KnightMovesGen.java
@@ -1,14 +1,25 @@
+import java.util.List;
 public class KnightMovesGen extends MovesGenerator {
+    public KnightMovesGen () {
+
+    }
     /**
-     * Constructor creates list of possible moves a knight
+     * Creates a list of possible moves a knight
      * can make which is later refined by PostMoveValidator.
      * Checks if knight can move in an L-shape vertically or
      * horizontally in either direction. Any moves which are
      * not on the board or are occupied by a friendly piece
      * are removed from the list. King checks are NOT considered.
      */
-    public KnightMovesGen (Board board, Piece knight) {
-        // implementation
-        // calls removeInvalid method from parent class to remove invalid moves
+    @Override
+    public List<Move> generate (Board board, Piece knight) {
+        int[] pos = knight.getCoords(); // get position of knight
+        int[][] add = {{2, 1}, {2, -1}, {1, 2}, {1, 2}, {1, -2}, {-1, 2}, {-1, -2}, {-2, 1}, {-2, -1}}; // to be added to pos
+        for (int i = 0; i <= 7; i++) {
+            this.moves.add(new Move(pos, new int[]{pos[0] + add[i][0], pos[1] + add[i][1]})); // adds new Move instance to moves
+        }
+        super.removeInvalid(this.moves, board);
+        return this.moves;
+
     }
 }
