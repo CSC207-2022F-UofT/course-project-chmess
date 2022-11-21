8c3ba18e4567d32d81d5903b086825eaaaadb508
Aditya Rao <adityakrao.projects@gmail.com>
2022-11-20 20:27:09 -0500

    Piece Copier Class Skeleton

--- src/main/java/PieceCopier.java
+++ src/main/java/PieceCopier.java
@@ -8,52 +8,14 @@ public class PieceCopier {
      * @return a clone of the board
      */
 
-    private Board newBoard;
-    private Board oldBoard = new Board();
+    private Piece newPiece;
+    private Piece oldPiece = new Piece();
 
-    public PieceCopier() {}
+    public PieceCopier(Piece oldPiece) {}
 
-    public Board createCopy(Board board) {
-        this.oldBoard = board;
-        Player[] oldPlayers = this.oldBoard.getPlayers();
-        clonePlayer(oldPlayers);
-        clonePieces();
+    public Piece createCopy(Piece piece) {
         //TODO!!!
-        // Don't entirely understand the use of mirror coordinates
-        // If still needed, will need to implement later
-
-        return this.newBoard;
+        return this.newPiece;
     }
 
-    private void clonePieces() {
-        for (int i=0; i<Board.HEIGHT; i++) {
-            for (int j=0; j<Board.WIDTH; j++) {
-                Piece absPiece = oldBoard.getPieceAtAbsCoords(i, j);
-                Piece relPiece = oldBoard.getPieceAtRelCoords(i, j);
-                Piece mirrorPiece = oldBoard.getPieceAtMirrorCoords(i, j);
-
-                if (absPiece != null) {
-                    Piece newPiece = absPiece.copy();
-                    newBoard.setPieceAtAbsCoords(i, j, newPiece);
-                }
-                
-                if (relPiece != null) {
-                    Piece newPiece = relPiece.copy();
-                    newBoard.setPieceAtRelCoords(i, j, newPiece);
-                }
-                 
-                if (mirrorPiece != null) {
-                    Piece newPiece = mirrorPiece.copy();
-                    newBoard.setPieceAtMirrorCoords(i, j, newPiece);
-                } 
-            }
-        }
-    }
-
-    private void clonePlayer(Player[] players) {
-        Player player1 = players[0].copy();
-        Player player2 = players[1].copy();
-        Player[] newPlayers = {player1, player2};
-        this.newBoard.setPlayers(newPlayers);
-    }
 }
