6bc7ce2fcbf0098969f58a7414beaf6ec8020f78
gurma <gurman.sachdeva@mail.utoronto.ca>
2022-11-22 14:06:49 -0500

    Revised implementation of individual movesgens. Implemented MovesGenerator

--- src/main/java/engine/movegen/MovesGenerator.java
+++ src/main/java/engine/movegen/MovesGenerator.java
@@ -1,36 +1,118 @@
+import java.util.ArrayList;
 import java.util.List;
+
 /**
  * Abstract class containing useful methods used in
  * its subclasses. Each subclass will generate a
  * list of possible moves according to the piece
- * it's related to, and will then call this method
- * removeInvalid to remove moves not on board or
- * occupied by friendly pieces.
+ * it's related to.
  */
 public abstract class MovesGenerator {
     public List<Move> moves;
     public abstract List<Move> generate (Board board, Piece piece);
     /**
      * Mutates list of moves by removing moves not on
-     * board or occupied by friendly pieces.
+     * board and are not occupied by friendly pieces.
+     *
+     * @param moves the list of moves being checked
+     * @param board the board on which these moves exist
      */
     public void removeInvalid (List<Move> moves, Board board) {
-        // TODO: will use checkTile method
+        for(Move move : moves){
+            if(!(checkTile(move, board))){
+                moves.remove(move);
+            }
+        }
     }
+
     /**
      * Returns true if the destination of move is on
      * board and is NOT occupied by friendly piece.
+     *
+     * @param move the move whose destination tile is being checked
+     * @param board the board on which the move exists
+     * @return whether the
      */
     public boolean checkTile (Move move, Board board) {
-        // TODO: will use methods isOccupiedByFriendly and containsAbsCoords (the latter from board class)
-        return true;
+        //returns false if the destination tile is not on the board
+        if(!(board.containsAbsCoords(move.destination[0], move.destination[1]))){return false;}
+        //checks if there is no piece on the destination tile
+        if(board.getPieceAtAbsCoords(move.destination[0], move.destination[1]) == null){
+            //there is no piece on the destination tile, so the move has no issue.
+            return true;
+        } else return isOccupiedByEnemy(move, board); //move is valid only if enemy is on destination tile
     }
+
     /**
      * Checks to see whether destination of move is already
      * occupied by a friendly piece.
+     *
+     * @param move the move whose destination tile is being checked
+     * @param board the board on which the move exists
+     * @return whether the destination tile is occupied by a friendly piece
      */
-    public boolean isOccupiedByFriendly (Move move) {
-        // TODO
-        return true;
+    public boolean isOccupiedByFriendly (Move move, Board board) {
+        //ensure destination tile is occupied
+        if(board.getPieceAtAbsCoords(move.destination[0], move.destination[1]) != null){
+            Piece movingPiece = board.getPieceAtAbsCoords(move.origin[0], move.origin[1]);
+            Piece destPiece = board.getPieceAtAbsCoords(move.destination[0], move.destination[1]);
+            //returns true if moving and destination pieces have same colours
+            return(movingPiece.getPlayer().getColor() == destPiece.getPlayer().getColor());
+        }
+        //destination tile is not occupied
+        return false;
+    }
+    /**
+     * Checks to see whether destination of move is
+     * occupied by an enemy piece.
+     *
+     * @param move the move whose destination tile is being checked
+     * @param board the board on which the move exists
+     * @return whether the destination tile is occupied by an enemy piece
+     */
+    public boolean isOccupiedByEnemy (Move move, Board board) {
+        //ensure destination tile is occupied
+        if(board.getPieceAtAbsCoords(move.destination[0], move.destination[1]) != null){
+            Piece movingPiece = board.getPieceAtAbsCoords(move.origin[0], move.origin[1]);
+            Piece destPiece = board.getPieceAtAbsCoords(move.destination[0], move.destination[1]);
+            //returns true if moving and destination pieces have different colours
+            return(movingPiece.getPlayer().getColor() != destPiece.getPlayer().getColor());
+        }
+        //destination tile is not occupied
+        return false;
+    }
+
+    /**
+     * Generates moves in straight lines from an original tile in given directions. Includes captures.
+     * Does not include moves which land on friendly pieces, jump over pieces, or are off the board.
+     *
+     * @param origin the original coordinates of the piece for which moves are being generated
+     * @param directions the directions of moves to be generated, represented by coordinate changes
+     * @param board the board on which moves are being generated
+     * @return a list of valid straight line moves in the specified directions
+     */
+    public List<Move> genStraightLineMoves (int[] origin, int[][] directions, Board board){
+        List<Move> moveList = new ArrayList<Move>();
+        for(int[] direction: directions){
+            //loop through new tiles (up to the board's dimensions, i.e. 7) one tile at a time.
+            //the first tile which cannot be moved onto indicates that no further
+            //tile can be moved to, since it will either be jumping over pieces
+            //or off the board. Hence, terminate the loop when this happens.
+            for(int distance = 1; distance < Board.WIDTH; distance++){
+                int[] destination = {origin[0] + distance * direction[0], origin[1] + distance * direction[1]};
+                Move toAdd = new Move(origin, destination);
+                //only add the move if it's valid, meaning the destination tile is on the board and no
+                //friendly piece is on it
+                if(checkTile(toAdd, board)){
+                    moveList.add(toAdd);
+                    //if the move is a capture, any further tile would be a jump, so stop looping
+                    //in this direction
+                    if(isOccupiedByEnemy(toAdd, board)){break;}
+                }
+                //the move lands on a friendly or is off the board, so stop looping for this direction
+                else{break;}
+            }
+        }
+        return moveList;
     }
 }
