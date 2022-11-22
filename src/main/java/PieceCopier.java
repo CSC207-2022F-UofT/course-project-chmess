600105ea86cd28b730b7df64a528af0c65fcfd1c
Aditya Rao <adityakrao.projects@gmail.com>
2022-11-21 19:11:44 -0500

    Piece Copier updated

--- src/main/java/PieceCopier.java
+++ src/main/java/PieceCopier.java
@@ -17,13 +17,13 @@ public class PieceCopier {
 
     public Piece createCopy() {
         String type = this.oldPiece.getType();
-        Player player = this.oldPiece.getPlayer();
+        char color = this.oldPiece.getColor();
         int[] coords = this.oldPiece.getCoords();
         int pointValue = this.oldPiece.getPointValue();
         MovesGenerator mg = this.oldPiece.getMovesGenerator();
 
         this.newPiece.setType(type);
-        this.newPiece.setPlayer(player);
+        this.newPiece.setColor(color);
         this.newPiece.setCoords(coords[0], coords[1]);
         this.newPiece.setPointValue(pointValue);
         this.newPiece.setMovesGenerator(mg);
