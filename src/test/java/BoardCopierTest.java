53433eeefd42dab27922e3d7ecfcbdd00b125eab
adirao-projects <adi.rao@mail.utoronto.ca>
2022-11-30 20:10:25 -0500

    BoardCopierTest and PieceFactory minor change

--- src/test/java/BoardCopierTest.java
+++ src/test/java/BoardCopierTest.java
@@ -6,5 +6,45 @@ import engine.entities.Piece;
 import engine.entities.PieceFactory;
 
 class BoardCopierTest {
+    @Test
+    void testCopy() {
+        Board board = new Board();
+        PieceFactory pf = new PieceFactory();
 
+        Piece piece = pf.create("P", 'w', 0, 0);
+        board.setPieceAtAbsCoords(0, 0, piece);
+
+        Board copy = board.copy();
+
+        Piece piece_original = board.getPieceAtAbsCoords(0, 0);
+        Piece piece_copy = copy.getPieceAtAbsCoords(0, 0);
+
+        Assertions.assertEquals(piece_original.getCoords(), piece_copy.getCoords());
+    }
+
+    @Test
+    void testNewInstance() {
+        Board board = new Board();
+        PieceFactory pf = new PieceFactory();
+
+        Piece piece = pf.create("P", 'w', 0, 0);
+        board.setPieceAtAbsCoords(0, 0, piece);
+
+        Board copy = board.copy();
+
+        Assertions.assertNotSame(board, copy);
+    }
+
+    @Test
+    void testPieceNewInstance() {
+        Board board = new Board();
+        PieceFactory pf = new PieceFactory();
+
+        Piece piece = pf.create("P", 'w', 0, 0);
+        board.setPieceAtAbsCoords(0, 0, piece);
+
+        Board copy = board.copy();
+        
+        Assertions.assertNotSame(board.getPieceAtAbsCoords(0,0), copy.getPieceAtAbsCoords(0,0));
+    }
 }
