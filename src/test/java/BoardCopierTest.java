c48adc94890fd6a7ad2ed235354bd93ae19f4b9d
Ryan Nguyen <ry.nguyen@mail.utoronto.ca>
2022-12-01 18:08:56 -0500

    Fixed typos in BoardCopierTest

--- src/test/java/BoardCopierTest.java
+++ src/test/java/BoardCopierTest.java
@@ -4,11 +4,12 @@ import org.junit.jupiter.api.Test;
 import engine.entities.Board;
 import engine.entities.Piece;
 import engine.entities.PieceFactory;
+import engine.entities.Player;
 
 class BoardCopierTest {
     @Test
-    void testCopy() {
-        Board board = new Board();
+    void testCopyPieces() {
+        Board board = createEmptyBoard();
         PieceFactory pf = new PieceFactory();
 
         Piece piece = pf.create("pawn", 'W');
@@ -20,12 +21,14 @@ class BoardCopierTest {
         Piece piece_original = board.getPieceAtAbsCoords(0, 0);
         Piece piece_copy = copy.getPieceAtAbsCoords(0, 0);
 
-        Assertions.assertSame(piece_original.getCoords(), piece_copy.getCoords());
+        Assertions.assertSame(piece_original.getColor(), piece_original.getColor());
+        Assertions.assertSame(piece_original.getCoords()[0], piece_copy.getCoords()[0]);
+        Assertions.assertSame(piece_original.getCoords()[1], piece_copy.getCoords()[1]);
     }
 
     @Test
     void testNewInstance() {
-        Board board = new Board();
+        Board board = createEmptyBoard();
         PieceFactory pf = new PieceFactory();
 
         Piece piece = pf.create("pawn", 'W');
@@ -39,15 +42,24 @@ class BoardCopierTest {
 
     @Test
     void testPieceNewInstance() {
-        Board board = new Board();
+        Board board = createEmptyBoard();
         PieceFactory pf = new PieceFactory();
 
-        Piece piece = pf.create("pawn", 'Q');
+        Piece piece = pf.create("pawn", 'W');
         piece.setCoords(0,0);
         board.setPieceAtAbsCoords(0, 0, piece);
 
         Board copy = board.copy();
         
         Assertions.assertNotSame(board.getPieceAtAbsCoords(0,0), copy.getPieceAtAbsCoords(0,0));
+    }
+
+    public Board createEmptyBoard() {
+        Board board = new Board();
+        Player p1 = new Player("Player 1", 'W');
+        Player p2 = new Player("Player 2", 'B');
+        Player[] players = {p1, p2};
+        board.setPlayers(players);
+        return board;
     }
 }
