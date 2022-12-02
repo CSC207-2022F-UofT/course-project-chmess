f2d68ae5a2a23ca4592b822c406ff4f5258057d7
adirao-projects <adi.rao@mail.utoronto.ca>
2022-12-02 15:27:19 -0500

    DisplayGame.java implemented

--- src/main/java/Interface/DisplayGame.java
+++ src/main/java/Interface/DisplayGame.java
@@ -1,6 +1,9 @@
 package Interface;
 
 import engine.entities.Game;
+import engine.entities.Board;
+import engine.entities.Piece;
+import engine.entities.Player;
 
 public class DisplayGame implements DisplayGameInt{
     /**
@@ -8,6 +11,53 @@ public class DisplayGame implements DisplayGameInt{
      * list of possible moves
      */
     public void display(Game game) {
-
+        Board board = game.getBoard();
+        board.selectDefaultPov();
+        for (int y=0; y<Board.HEIGHT; y++) {
+            for (int x=0; x<Board.WIDTH; x++) {
+                Piece piece = board.getPieceAtRelCoords(x, y);
+                if (piece == null) {
+                    System.out.print(".");
+                } else {
+                    String type = piece.getType();
+                    char pieceChar;
+                    switch (type) {
+                        case "pawn":
+                            pieceChar = 'p';
+                            break;
+                        case "knight":
+                            pieceChar = 'n';
+                            break;
+                        case "bishop":
+                            pieceChar = 'b';
+                            break;
+                        case "rook":
+                            pieceChar = 'r';
+                            break;
+                        case "queen":
+                            pieceChar = 'q';
+                            break;
+                        case "king":
+                            pieceChar = 'k';
+                            break;
+                        default:
+                            pieceChar = '.';
+                            break;
+                    }
+                    if (piece.getColor() == 'W') {
+                        System.out.print(Character.toUpperCase(pieceChar));
+                    } else {
+                        System.out.print(pieceChar);
+                    }
+                }
+            }
+        System.out.print("\n");
+        }
+        Player currentPlayer = board.getCurrentPlayer();
+        if (currentPlayer.getColor()=='W') {
+            System.out.println("White to move...");
+        } else {
+            System.out.println("Black to move...");
+        }
     }
 }
