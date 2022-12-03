ba4d94c092d962144fb79b29651778afda6e1966
Ryan Nguyen <ry.nguyen@mail.utoronto.ca>
2022-12-03 00:05:32 -0500

    Write tests for PostMoveValidator

--- src/test/java/PostMoveValidatorTest.java
+++ src/test/java/PostMoveValidatorTest.java
@@ -18,4 +18,74 @@ public class PostMoveValidatorTest extends MoveTest {
 
         Assertions.assertFalse(pmv.moveIsValid(board, move));
     }
+
+    @Test
+    public void DiscoveredCheckIsInvalid() {
+        Board board = createEmptyBoard('W');
+
+        createAndPlacePiece("king", 'W', board, 0, 0);
+        createAndPlacePiece("rook", 'W', board, 0, 1);
+        createAndPlacePiece("rook", 'B', board, 0, 7);
+        Move move = new Move(new int[]{0, 1}, new int[]{1, 1});
+
+        Assertions.assertFalse(pmv.moveIsValid(board, move));
+    }
+
+    @Test
+    public void DirectCheckToDirectCheckIsInvalid() {
+        Board board = createEmptyBoard('W');
+
+        createAndPlacePiece("king", 'W', board, 0, 0);
+        createAndPlacePiece("rook", 'B', board, 7, 0);
+        createAndPlacePiece("rook", 'B', board, 1, 7);
+        Move move = new Move(new int[]{0, 0}, new int[]{1, 1});
+
+        Assertions.assertFalse(pmv.moveIsValid(board, move));
+    }
+
+    @Test
+    public void DodgeDirectCheckValid() {
+        Board board = createEmptyBoard('W');
+
+        createAndPlacePiece("king", 'W', board, 0, 0);
+        createAndPlacePiece("knight", 'B', board, 1, 2);
+        Move move = new Move(new int[]{0, 0}, new int[]{1, 0});
+
+        Assertions.assertTrue(pmv.moveIsValid(board, move));
+    }
+
+    @Test
+    public void BlockDirectCheckIsValid() {
+        Board board = createEmptyBoard('W');
+
+        createAndPlacePiece("king", 'W', board, 0, 0);
+        createAndPlacePiece("rook", 'W', board, 1, 1);
+        createAndPlacePiece("rook", 'B', board, 0, 7);
+        Move move = new Move(new int[]{1, 1}, new int[]{0, 1});
+
+        Assertions.assertTrue(pmv.moveIsValid(board, move));
+    }
+
+    @Test
+    public void CaptureDirectCheckIsValid() {
+        Board board = createEmptyBoard('W');
+
+        createAndPlacePiece("king", 'W', board, 0, 0);
+        createAndPlacePiece("bishop", 'W', board, 1, 1);
+        createAndPlacePiece("bishop", 'B', board, 2, 2);
+        Move move = new Move(new int[]{1, 1}, new int[]{2, 2});
+
+        Assertions.assertTrue(pmv.moveIsValid(board, move));
+    }
+
+    @Test
+    public void KingCaptureDirectCheckIsValid() {
+        Board board = createEmptyBoard('W');
+
+        createAndPlacePiece("king", 'W', board, 0, 0);
+        createAndPlacePiece("bishop", 'B', board, 1, 1);
+        Move move = new Move(new int[]{0, 0}, new int[]{1, 1});
+
+        Assertions.assertTrue(pmv.moveIsValid(board, move));
+    }
 }
